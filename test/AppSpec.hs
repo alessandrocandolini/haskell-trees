b304e3dce24fa619a36efc30917afc0f945c53f6
Alessandro Candolini <alessandrocandolini@gmail.com>
2022-11-09 02:01:49 +0000

    better support for nonempty, and rose trees

--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE QuasiQuotes #-}
 module AppSpec where
 
-import App (pretty, one, basic, more)
+import App (pretty, one, basic1, basic2, basic3, more)
 import Test.Hspec ( describe, it, shouldBe, Spec )
 import Test.Hspec.QuickCheck ( prop )
 import Test.QuickCheck.Property ()
@@ -11,25 +11,25 @@ spec :: Spec
 spec = describe "Simple test" $ do
 
      it "Example 1 (one leaf)" $
-        pretty basic `shouldBe` [trimming|
+        pretty one `shouldBe` [trimming|
         1
         |]
 
      it "Example 2 (one node, one leaf)" $
-        pretty basic `shouldBe` [trimming|
+        pretty basic1 `shouldBe` [trimming|
            1
            -2
           |]
      
      it "Example 3 (one node, one branch, one leaf)" $
-        pretty basic `shouldBe` [trimming|
+        pretty basic2 `shouldBe` [trimming|
            1
            -2
            --3
           |]
 
      it "Example 4 (one node, two leafs)" $
-        pretty basic `shouldBe` [trimming|
+        pretty basic3 `shouldBe` [trimming|
            1
            -2
            -3
