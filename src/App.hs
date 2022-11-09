a7582a016f62529661c6db43a34780ae5ad84ae9
Alessandro Candolini <alessandrocandolini@gmail.com>
2022-11-09 01:09:02 +0000

    flash out some of the data models

--- src/App.hs
+++ src/App.hs
@@ -1,4 +1,38 @@
+{-# LANGUAGE DerivingVia #-}
 module App where
+import Data.Text as T
+import Control.Monad.State
+
 
 program :: IO ()
 program = putStrLn "hello!"
+
+-- Update to non empty
+data Tree a = Leaf a | Node a [Tree a] deriving (Eq, Show)
+
+one:: Tree Int
+one = Leaf 1
+
+basic :: Tree Int
+basic = Node 1 [Leaf 2, Leaf 3]
+
+more :: Tree Int
+more = Node 1 [ Node 2 [Leaf 3, Leaf 4], Node 5 [Leaf 6, Node 7 [Leaf 8, Leaf 9, Leaf 10]]]
+
+separator :: String
+separator = "-"
+
+newtype Depth = Depth Int deriving (Eq,Show)
+   deriving Num via Int
+
+newtype Label a = Label a deriving (Eq,Show)
+
+
+labelling :: Tree a -> State Depth (Tree (Label a))
+labelling (Leaf a) = pure $ Leaf (Label a)
+labelling (Node a bs) = undefined
+
+
+pretty :: Show a => Tree a -> Text
+pretty (Leaf a) = pack $ show a
+pretty (Node a _) = pack $ show a
--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -1,15 +1,54 @@
+{-# LANGUAGE QuasiQuotes #-}
 module AppSpec where
 
-import App
-import Test.Hspec
-import Test.Hspec.QuickCheck
-import Test.QuickCheck.Property
+import App (pretty, one, basic, more)
+import Test.Hspec ( describe, it, shouldBe, Spec )
+import Test.Hspec.QuickCheck ( prop )
+import Test.QuickCheck.Property ()
+import NeatInterpolation
 
 spec :: Spec
 spec = describe "Simple test" $ do
 
-     it "example-based unit test" $
-        1 `shouldBe` 1
+     it "Example 1 (one leaf)" $
+        pretty basic `shouldBe` [trimming|
+        1
+        |]
+
+     it "Example 2 (one node, one leaf)" $
+        pretty basic `shouldBe` [trimming|
+           1
+           -2
+          |]
+     
+     it "Example 3 (one node, one branch, one leaf)" $
+        pretty basic `shouldBe` [trimming|
+           1
+           -2
+           --3
+          |]
+
+     it "Example 4 (one node, two leafs)" $
+        pretty basic `shouldBe` [trimming|
+           1
+           -2
+           -3
+          |]
+
+
+     it "Example 5 (a more complex, nested tree)" $
+        pretty more `shouldBe` [trimming|
+           1
+           -2
+           --3
+           --4
+           -5
+           --6
+           --7
+           ---8
+           ---9
+           ---10
+           |]
 
      prop "property-based unit test" $
         \l -> reverse ( reverse l ) == ( l::[Int])
